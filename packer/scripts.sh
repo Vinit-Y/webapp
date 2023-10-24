7787b82b4587b5dc8f7dcec1cf73401941e4807e
Vinit V Yadav <122958392+Vinit-Y@users.noreply.github.com>
2023-10-24 19:52:19 -0400

    Ass6

--- packer/scripts.sh
+++ packer/scripts.sh
@@ -10,15 +10,15 @@ node -v
 npm -v
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install mariadb-server -y
-echo "GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;" | sudo mariadb
-echo "FLUSH PRIVILEGES;" | sudo mariadb
-echo "show databases;" | sudo mariadb
-echo "create database mydb;" | sudo mariadb
-echo "show databases;" | sudo mariadb
-echo "exit" | sudo mariadb
-sudo systemctl status mariadb
-sudo mysqladmin version
+# sudo apt install mariadb-server -y
+# echo "GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;" | sudo mariadb
+# echo "FLUSH PRIVILEGES;" | sudo mariadb
+# echo "show databases;" | sudo mariadb
+# echo "create database mydb;" | sudo mariadb
+# echo "show databases;" | sudo mariadb
+# echo "exit" | sudo mariadb
+# sudo systemctl status mariadb
+# sudo mysqladmin version
 sudo mkdir webapp
 sudo unzip webapp.zip -d webapp
 echo "------------Unziped File Successfully--------------"
@@ -28,9 +28,9 @@ ls -al
 cd webapp/ || exit
 pwd
 ls -al
-sudo npm install  -y
-ls -al
-sudo cat .env
+# sudo npm install  -y
+# ls -al
+# sudo cat .env
 
 
 
