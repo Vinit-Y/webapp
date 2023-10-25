90be6c755b2a1f98fef465f0e9346d789db584c0
Vinit V Yadav <122958392+Vinit-Y@users.noreply.github.com>
2023-10-25 01:29:03 -0400

    Modified files

--- packer/scripts.sh
+++ packer/scripts.sh
@@ -3,30 +3,27 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get clean
+
 sudo apt install unzip -y
 unzip -v
-sudo apt install nodejs npm -y
+
+sudo apt-get install npm -y
+sudo apt-get install nodejs -y
 node -v
 npm -v
-sudo apt update -y
-sudo apt upgrade -y
-# sudo apt install mariadb-server -y
-# echo "GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;" | sudo mariadb
-# echo "FLUSH PRIVILEGES;" | sudo mariadb
-# echo "show databases;" | sudo mariadb
-# echo "create database mydb;" | sudo mariadb
-# echo "show databases;" | sudo mariadb
-# echo "exit" | sudo mariadb
-# sudo systemctl status mariadb
-# sudo mysqladmin version
+
 pwd
 ls -al
+
 sudo cp /tmp/webapp.zip /opt/webapp.zip
-cd /opt/ || exit
+
+cd /opt || exit
 sudo mkdir webapp
 sudo unzip webapp.zip -d webapp
+
 echo "------------Unziped File Successfully--------------"
 sudo apt-get remove -y git
+
 pwd
 ls -al
 cd webapp/ || exit
