a73fb486b460e7ee82fe5910485197e054050d41
Vinit <yadav.vini@northeastern.edu>
2023-11-08 16:55:31 -0500

    Bug fixes

--- api/utils/response.js
+++ api/utils/response.js
@@ -1,12 +1,11 @@
 import logger from './logger.js';
-import { logger1 } from './logger.js';
 export const setResponse = async (req, res, status, data) => {
 
   const endpoint = req.originalUrl; 
   if (status >= 200 && status < 300) {
-    logger1.info(`Request to [${endpoint}] was successful. Status code: ${status}`);
+    logger.info(`Request to [${endpoint}] was successful. Status code: ${status}`);
   } else if (status >= 400 && status < 500) {
-    logger1.warn(`Client error on request to [${endpoint}]. Status code: ${status}`);
+    logger.warn(`Client error on request to [${endpoint}]. Status code: ${status}`);
   } else if (status >= 500) {
     logger.error(`Server error on request to [${endpoint}]. Status code: ${status}`);
   }
