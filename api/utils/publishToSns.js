6941a8f4b80b03e44a2a21e2615006dc5aaf29f3
Vinit <yadav.vini@northeastern.edu>
2023-12-03 00:51:32 -0500

    Modified Workflow

--- api/utils/publishToSns.js
+++ api/utils/publishToSns.js
@@ -5,8 +5,6 @@ dotenv.config();
 
 export const publishMessageToSns = async (message) => {
   AWS.config.update({ 
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION 
   });
 
